49199e5c04cb6f11c18843022c9ae03cc7e11b1b
Tanvir-Hossain847 <tanvirhossaintufa@gmail.com>
2025-11-26 16:59:50 +0600

    add express server connect mongodb add post oparation

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = "mongodb+srv://zap_shift:owiaXGxzPPNIudBI@tanvir369.ymezqkm.mongodb.net/?appName=Tanvir369";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('this is my running server for zap-shift')
+})
+
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const zap_shift = client.db("zap_shift")
+    const collection = zap_shift.collection("parcel")
+
+    app.post('/parcelInfo', async(req, res) => {
+        const newParcel = req.body;
+        const result = await collection.insertOne(newParcel)
+        res.send(result)
+        console.log("the result is", result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// owiaXGxzPPNIudBI
+// zap_shift
